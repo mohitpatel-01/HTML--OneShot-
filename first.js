f7dfdb91fae52b4e17f70dd5d99927960ce1615f
mohitpatel-o1 <spmohit01@gmail.com>
2024-07-24 10:10:40 +0530

    chane

--- first.js
+++ first.js
@@ -3,4 +3,4 @@ let num = prompt("Guess the Number");
 while (num != gameNumber) {
     num = prompt("you entered a wrong no. pls try again");
 }
-console.log("Congrets you enter a right no.");
+console.log("Congrets you enter a right no.6");
